27d1d2c4ebaf010cf26a3c3ac5205ed12a8f1ee6
tshellenberger <teddyshellenberger@gmail.com>
2024-04-18 16:59:43 -0700

    backend files

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -33,6 +34,7 @@ const users = {
   ]
 };
 
+app.use(cors());
 app.use(express.json());
 
 const findUserByName = (name) => {
@@ -50,6 +52,30 @@ app.get("/users", (req, res) => {
   } else {
     res.send(users);
   }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.get("/", (req, res) => {
