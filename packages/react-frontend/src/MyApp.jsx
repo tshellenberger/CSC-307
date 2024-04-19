8c55e5992a4f819fb38d75aa28ff43048042b14f
tshellenberger <teddyshellenberger@gmail.com>
2024-04-18 17:01:37 -0700

    IE3 start

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,8 +1,9 @@
 // src/MyApp.jsx
 //import React from "react";
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import Table from "./Table";
 import Form from "./Form";
+import React, {useState, useEffect} from 'react';
 
 
 function MyApp() {
@@ -16,8 +17,36 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function updateList(person) { 
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+	      .then((res) => res.json())
+	      .then((json) => setCharacters(json["users_list"]))
+	      .catch((error) => { console.log(error); });
+  }, [] );
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
   }
 
   return (
